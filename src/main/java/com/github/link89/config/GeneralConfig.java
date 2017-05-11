bcdc90a91947bd8eeab4627baeae9f78e07c38b5
Weihong Xu <xuweihong02@meituan.com>
2017-05-11 20:47:27 +0800

    test success when @EnableSpringConfigured is set

--- src/main/java/com/github/link89/config/GeneralConfig.java
+++ src/main/java/com/github/link89/config/GeneralConfig.java
@@ -1,7 +1,9 @@
 package com.github.link89.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 
 @Configuration
+@EnableSpringConfigured
 public class GeneralConfig {
 }
